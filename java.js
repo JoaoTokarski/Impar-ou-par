4b0d0ed8a24c45c58a5f1aecf41e54e1333cf13a
JoaoTokarski <168202255+JoaoTokarski@users.noreply.github.com>
2025-03-28 14:06:42 +0000

    5

--- java.js
+++ java.js
@@ -1,13 +1,14 @@
 function verificarParOuImpar() {
     const num1 = parseInt(document.getElementById("num1").value);
+    const num2 = parseInt(document.getElementById("num2").value);
 
     if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById("resultado").innerText = "Por favor, insira um número válidos.";
+        document.getElementById("resultado").innerText = "Por favor, insira dois números válidos.";
         document.getElementById("resultado").classList.remove("par", "impar");
         return;
     }
 
-
+    const soma = num1 + num2;
     let resultado = (soma % 2 === 0) ? "A soma é Par!" : "A soma é Ímpar!";
     
     const resultadoDiv = document.getElementById("resultado");
